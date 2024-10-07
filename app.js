534cd1bbed9cfa5275cbffed9a29f76e2060541c
Šenekl Ondřej <L20220054@SSPTAJI.CZ>
2024-10-07 13:09:55 +0200

    app update

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ if ('serviceWorker' in navigator) {
 function sayHello() {
     document.getElementById('hello').innerText = 'Hello world';
 }
+
+function Calculate()
+        {
+            const a = document.getElementById("a").value;
+            const b = document.getElementById("b").value;
+            const c = document.getElementById("c").value;
+
+            let vysledek1;
+            let vysledek2;
+
+            let disc = (b * b) - (4*a*c);
+            if (disc >= 0)
+            {
+                if (d = 0)
+                {
+                    vysledek1 = (-b) / (2*a);
+                    vysledek2 = (-b) / (2*a);
+                }
+            
+                vysledek1 = (-b+Math.sqrt(disc)) / (2*a*b);
+                vysledek2 = (-b-Math.sqrt(disc)) / (2*a*b);
+            }
+            else 
+            {
+                vysledek1 = "nelze najit vysledek v poli realnych cisel!";
+            }
+
+            document.getElementById('x1').innerText = 
+        }
